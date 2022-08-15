28ff79a3637ea2903652aa1a5adec032ffeb3169
Valsept <70474120+Valsept@users.noreply.github.com>
2022-08-15 14:57:26 +0200

    Update and rename pr7.py to pe7.py

--- pe7.py
+++ pe7.py
@@ -1,23 +1,24 @@
 #Problem 7 from Euler Project
 #10001st prime
+#Completed on Tue, 14 Dec 2021, 12:14
 
 def is_prime (nomber):
     if nomber == 1:
         return False
     if nomber%2 == 0:
         return False
-    for i in range(3, int(nomber**0.5)+1, 1):
+    for i in range(3, int(nomber**0.5)+1, 2):
         if nomber%i == 0:
             return False
     return True
 
-number = 0
-position = 0
-while position <= 10001:
-    number += 1
+number = 1
+position = 1
+
+while position != 10001:
+    number += 2
     if is_prime(number):
         position += 1
 print(number)
-
 
 #Check if it divide by 2 / 3 / 5 
